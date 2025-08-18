b926e3a129b0f279a4024804c03a4a95460dea00
Tamiron Post <tdrozdikova@gmail.com>
2025-08-19 00:31:45 +0300

    пофиксили IntegrityError при социальной и локальной регистрации

--- Coder/Coder/settings.py
+++ Coder/Coder/settings.py
@@ -168,6 +168,20 @@ SOCIAL_AUTH_GITHUB_SCOPE = ['user:email']
 SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = os.getenv('AUTH_GOOGLE_KEY')
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = os.getenv('AUTH_GOOGLE_SECRET')
 
+# Настройки social-auth
+SOCIAL_AUTH_PIPELINE = (
+    'social_core.pipeline.social_auth.social_details',
+    'social_core.pipeline.social_auth.social_uid',
+    'social_core.pipeline.social_auth.auth_allowed',
+    'social_core.pipeline.social_auth.social_user',
+    'users.pipeline.associate_by_email',
+    'social_core.pipeline.user.get_username',
+    'social_core.pipeline.user.create_user',
+    'social_core.pipeline.social_auth.associate_user',
+    'social_core.pipeline.social_auth.load_extra_data',
+    'social_core.pipeline.user.user_details',
+)
+
 # Настройки почты для mail.ru
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.mail.ru'  # SMTP-сервер mail.ru
---
+++ Coder/users/pipeline.py
@@ -0,0 +1,31 @@
+from django.shortcuts import redirect
+from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+from django.db import IntegrityError
+
+
+def associate_by_email(backend, details, user=None, *args, **kwargs):
+    """
+    Ассоциирует аккаунт социальной сети с существующим пользователем по электронной почте
+    или отображает сообщение об ошибке, если email уже зарегистрирован.
+    """
+    if user:
+        return None
+
+    email = details.get('email')
+    if email:
+        User = get_user_model()
+        try:
+            user = User.objects.get(email=email)
+            # Ассоциируем аккаунт социальной сети с существующим пользователем
+            return {'user': user, 'is_new': False}
+        except ObjectDoesNotExist:
+            try:
+                user = User.objects.create_user(username=email, email=email)
+                return {'user': user, 'is_new': True}
+            except IntegrityError:
+                messages.error(kwargs['request'], "Аккаунт с такой почтой уже зарегистрирован. Войдите, используя форму регистрации, или используйте другую почту для входа через Google.")
+                return redirect(reverse('users:login'))
+    return None
